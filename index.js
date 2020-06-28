e3be3e0b089dfdcf690a7edb2d236a4c2a032410
Heat-github <58354355+Heat-github@users.noreply.github.com>
2020-06-28 13:36:47 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+function resize_window() {
+    window.open ('http://www.w3cschool.cc/try/demo_source/trycss3_media_example1.htm','newwindow','height=299,width=299,top=30%,left=40%,toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no')
+}
