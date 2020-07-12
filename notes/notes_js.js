bc11fa12a18c563f1ad1f33b5e18d36d870eee15
Heat-github <alding.dong0@gmail.com>
2020-07-12 12:02:04 +0800

    夜间模式

--- notes/notes_js.js
+++ notes/notes_js.js
@@ -12,3 +12,30 @@ function cataTrans(){       //手机端目录展开与收合
     }
 }
 
+function darkMode(){
+    elet1 = document.getElementsByClassName("darkmode");
+    elet2 = document.getElementsByTagName("body");
+    bgc2 = window.getComputedStyle(elet2[0]).backgroundColor;
+    elet3 = document.getElementsByClassName("catalog");
+    elet4 = document.getElementsByClassName("note_tag");
+
+    if(bgc2.match("128")){
+        elet1[0].style.opacity = "1.0";
+        elet2[0].style.backgroundColor = "rgb(60,60,60)";
+        elet3[0].style.backgroundColor = "rgb(60,60,60)";
+        for(i=0;i<elet4.length;i++){
+            elet4[i].style.backgroundColor = "rgb(128,128,128)";
+            elet4[i].style.color = "rgb(255,255,255)";
+        }
+    }
+    else{
+        elet1[0].style.opacity = "0.5";
+        elet2[0].style.backgroundColor = "rgb(128,128,128)";
+        elet3[0].style.backgroundColor = "rgb(128,128,128)";
+        for(i=0;i<elet4.length;i++){
+            elet4[i].style.backgroundColor = "rgb(246, 253, 236)";
+            elet4[i].style.color = "rgb(0,0,0)";
+        }
+    }
+}
+
