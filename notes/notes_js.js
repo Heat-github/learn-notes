334971f14b0f189fb065685d9392e4f89716259a
Heat-github <alding.dong0@gmail.com>
2020-07-13 21:32:44 +0800

    选择器，jQuery设置手机端目录折叠

--- notes/notes_js.js
+++ notes/notes_js.js
@@ -1,16 +1,3 @@
-function cataTrans(){       //手机端目录展开与收合
-    x = screen.availWidth;
-    element = document.getElementById('acatalog');
-    if(x>1000){element.style.display="block";}
-    else{
-        if(element.style.display.match("none")){
-            element.style.display="block";
-        }
-        else{
-            element.style.display="none";
-        }
-    }
-}
 
 function modeFunc(){
     var time = new Date().getHours();
