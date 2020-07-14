7a3bc1fe4b6a2a160967b5780363574ceee6d1bc
Heat-github <alding.dong0@gmail.com>
2020-07-14 20:51:29 +0800

    jQuery事件

--- notes/notes_jQ.js
+++ notes/notes_jQ.js
@@ -3,10 +3,10 @@ $.noConflict();    //避免与其他语言的 $ 快捷方式冲突
 jQuery(document).ready(function($){
   //再将 $ 传给 ready ，在函数内使用 $ 符号也不会冲突
   //ready函数保证DOM加载完成在调用 jQuery 函数库
-
+  
   $(".catabutton").click(function(){    //手机端(小屏幕)目录展开与收合
-    browserWidth = document.documentElement.clientWidth;
-    if(browserWidth < 888){$("#acatalog").slideToggle(200);}
+    x = document.documentElement.clientWidth;
+    if(x < 888){$("#acatalog").slideToggle(200);}
     else{$("#acatalog").slideDown(200);}
   })
 T
