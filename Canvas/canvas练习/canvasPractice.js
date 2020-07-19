23a5d2001249cba2b08f3af0e44dcb3c2b0d41d7
Heat-github <alding.dong0@gmail.com>
2020-07-20 00:25:44 +0800

    canvas 圆弧

--- Canvas/canvas练习/canvasPractice.js
+++ Canvas/canvas练习/canvasPractice.js
@@ -1,8 +1,8 @@
 onload = function(){
     console.log("alex");
     console.log("Jenny");
-    canSize();
     initial();
+    setCanSize();
 }
 btnNum = new Array();
 function initial(){
@@ -10,41 +10,67 @@ function initial(){
     for(i=0;i<x.length;i++){btnNum[i]=0;}
 }
 
-function canSize(){
-    var cw = document.documentElement.clientWidth;
-    var ctt = document.getElementsByClassName("content");
-    var cttw = ctt[0].offsetWidth;
+function getSize(clsName){
+    var clsn = clsName;
+    var ccww = document.documentElement.clientWidth;
+    var cls = document.getElementsByClassName(clsn);
+    var clsw = cls[0].offsetWidth;
+    return [ccww,clsw];
+}
+
+function setCanSize(){
+    var widths = getSize("content");
+    var w = widths[1] - widths[0]*0.01 - 16;
+    var h = 9*w/16;
     var cvs = document.getElementsByClassName("canCss");
     for(i=0;i<cvs.length;i++){
         ctx = cvs[i].getContext("2d");
-        w = cttw - cw*0.01 - 16;
-        h = 9*w/16;
         ctx.canvas.width = w;
         ctx.canvas.height= h;
     }
+    return cvs.length;
+}
+
+function canFun0(){
+    btnNum[0] = 1;
+    var cw = document.documentElement.clientWidth;
+    var can = document.getElementById("can0");
+    can.style.border = "2px solid orange";
+    var ctx = can.getContext("2d");
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round"; 
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "blue";
+
+    ctx.beginPath();
+    ctx.moveTo(10,10);
+    ctx.lineTo(cw*0.3,50);
+    ctx.lineTo(50,80);
+    ctx.lineTo(10,10);
+    ctx.lineTo(cw*0.3,50);
+    ctx.stroke();
+    ctx.strokeRect(100,50,20,20);  
+    ctx.fillRect(120,20,20,20);
+    
+    ctx.beginPath();
+    ctx.rect(120,60,60,60);
+    ctx.fill();            //如果路径未关闭，那么 fill() 方法会从路径结束点到开始点之间添加一条线，以关闭该路径，然后填充该路径
 }
 
 function canFun1(){
-    btnNum[0] = 1;
-    var c1 = document.getElementById("can1");
-    c1.style.border = "2px solid orange";
-    var ctx1 = c1.getContext("2d");
-    ctx1.lineWidth = 5;
-    ctx1.lineCap = "round"; 
-    ctx1.strokeStyle = "red";
-    ctx1.fillStyle = "blue";
-
-    ctx1.beginPath();
-    ctx1.moveTo(10,10);
-    ctx1.lineTo(100,50);
-    ctx1.lineTo(50,80);
-    ctx1.lineTo(10,10);
-    ctx1.lineTo(100,50);
-    ctx1.stroke();
-    ctx1.strokeRect(100,50,20,20);  
-    ctx1.fillRect(120,20,20,20);
+    btnNum[1] = 1;
+    var can = document.getElementById("can1");
+    can.style.border = "2px solid orange";
+    var [cw,w] = getSize("content");
     
-    ctx1.beginPath();
-    ctx1.rect(120,60,60,60);
-    ctx1.fill();            //如果路径未关闭，那么 fill() 方法会从路径结束点到开始点之间添加一条线，以关闭该路径，然后填充该路径
+    var ctx = can.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(w*0.25,w*0.25,w*0.2,Math.PI/2,210*Math.PI/180,false);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(20,20);
+    ctx.lineTo(70,20);
+    ctx.arcTo(120,30,120,70,50);
+    ctx.lineTo(120,120);
+    ctx.stroke();
 }
--- Canvas/canvas练习/canvasPracticejQ.js
+++ Canvas/canvas练习/canvasPracticejQ.js
@@ -2,8 +2,17 @@ $.noConflict();
 jQuery(document).ready(function($){
 
   $(window).resize(function(){
-    canSize();
-    if(btnNum[0]){canFun1();}
+    setCanSize();
+    if(btnNum[0]){canFun0();}
+    if(btnNum[1]){canFun1();}
+    
+  });
+
+  $(".left>p").click(
+    function(){
+      if($(this).hover()){
+        $(this).next().slideToggle(100);
+      }
   });
 
 });
